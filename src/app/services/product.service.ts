8b2c6629abacda9da81b761fd2652bfff61e607a
mmunozc <mateomunozc@outlook.com>
2025-09-11 12:16:25 -0500

    console.log

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,41 +1,44 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Constant } from './const/constant';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+  private base = environment.apiBase;
+
   constructor(private http: HttpClient) { }
 
   getAllProducts() {
-    console.log('🌐 Endpoint:', `${Constant.API__END_POINT}/Login`);
-    return this.http.get(Constant.API__END_POINT + Constant.METHOD.GET_ALL_PRODUCTS)
+    console.log('🌐 Endpoint:', `${this.base}`);
+    return this.http.get(`${this.base}/` + Constant.METHOD.GET_ALL_PRODUCTS)
   }
 
   getAllCategories() {
-    return this.http.get(Constant.API__END_POINT + Constant.METHOD.GET_ALL_CATEGORY)
+    return this.http.get(`${this.base}/` + Constant.METHOD.GET_ALL_CATEGORY)
   }
 
   saveProduct(body: any) {
-    return this.http.post(Constant.API__END_POINT + Constant.METHOD.SAVE_PRODUCT, body)
+    return this.http.post(`${this.base}/` + Constant.METHOD.SAVE_PRODUCT, body)
   }
 
   updateProduct(body: any) {
-    return this.http.post(Constant.API__END_POINT + Constant.METHOD.UPDATE_PRODUCT, body)
+    return this.http.post(`${this.base}/` + Constant.METHOD.UPDATE_PRODUCT, body)
   }
 
   deleteProduct(id: any) {
-    return this.http.get(Constant.API__END_POINT + Constant.METHOD.DELETE_PRODUCT + id)
+    return this.http.get(`${this.base}/` + Constant.METHOD.DELETE_PRODUCT + id)
   }
 
   saveCategory(body: any) {
-    return this.http.post(Constant.API__END_POINT + Constant.METHOD.SAVE_CATEGORY, body)
+    return this.http.post(`${this.base}/` + Constant.METHOD.SAVE_CATEGORY, body)
   }
 
   deleteCategory(id: any) {
-    return this.http.get(Constant.API__END_POINT + Constant.METHOD.DELETE_CATEGORY + id)
+    return this.http.get(`${this.base}/` + Constant.METHOD.DELETE_CATEGORY + id)
   }
 
 
